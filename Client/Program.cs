6e39efcd37f58f12afaeaae709ffb78d6596f063
marvin.taylor288@gmail.com <marvin.taylor288@gmail.com>
2022-07-17 01:15:43 -0700

    base href added

--- Client/Program.cs
+++ Client/Program.cs
@@ -18,6 +18,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IMemberService, MemberService>();
