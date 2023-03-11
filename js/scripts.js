01861a382c52e62f07df41de9ed3ed991d83583b
marvintaylor-dev <76923383+marvintaylor-dev@users.noreply.github.com>
2023-03-11 19:09:03 +0000

    Deploying to master from development @ 337c0522bc532ff8bdc4bc4cab4317147440a4de 🚀

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+﻿
+function saveOnEnter() {
+    let saveBtn = document.querySelector("#saveButton");
+    let editBtn = document.querySelector("#editButton");
+
+
+    document.querySelector("#taskInput").addEventListener("keyup", function (event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter" && saveBtn) {
+            console.log("Clicked Enter with Save Button")
+            event.preventDefault();
+            // Trigger the button element with a click
+            saveBtn.click();
+        }
+        else if (event.key === "Enter" && editBtn) {
+            console.log("Clicked Enter with Edit Button.")
+            event.preventDefault();
+            editBtn.click()
+        }
+        else {
+            console.log("Clicked Enter with Nothing selected.")
+        }
+    });
+}
+
+
+
+function FocusAsync() {
+    //give time for taskInput to load
+    setTimeout(function () {
+        document.querySelector("#taskInput").focus();
+    }, 50)
+};
+
