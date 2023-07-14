1150c7417d1d195a3ded5b4bbbde6f43a659c674
Marvin T <mtdev88@gmail.com>
2023-07-14 13:37:27 -0700

    added organizationModelChanges

--- Shared/OrganizationModel.cs
+++ Shared/OrganizationModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,20 @@ namespace Tasky.Shared
 {
     public class OrganizationModel
     {
+        [Key]
         public int OrganizationId { get; set; }
+        [Required]
+        public int OwnerId { get; set; }
+
+        public List<Member> OrganizationMembers { get; set; } = new();
+
+        public List<User> UserMembers { get; set; } = new();
         public string OrganizationName { get; set; } = string.Empty;
+        public string OrganizationDescription { get; set; } = string.Empty;
+        
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set;} = DateTime.Now;
 
-        //add things like
-        //org description
-        //org team members
-        //etc
-
-
+        
     }
 }
