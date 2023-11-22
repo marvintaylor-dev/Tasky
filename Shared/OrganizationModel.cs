3a3d2bb743732836574b2d3310427a87de49df47
Marvin T <mtdev88@gmail.com>
2023-11-21 21:43:44 -0800

    Add tasks to Kanban board

--- Shared/OrganizationModel.cs
+++ Shared/OrganizationModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -15,6 +16,7 @@ namespace Tasky.Shared
         //[Required]
         public int? OwnerId { get; set; }
         //public List<Member> OrganizationMembers { get; set; } = new();
+
         //public List<User> OrganizationUsers { get; set; } = new();
         public string OrganizationName { get; set; } = string.Empty;
         public string OrganizationDescription { get; set; } = string.Empty;
--- Shared/User.cs
+++ Shared/User.cs
@@ -14,7 +14,9 @@ namespace Tasky.Shared
         public string Email { get; set; } = string.Empty;
 
         public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }        
+        public byte[] PasswordSalt { get; set; }
+
+
         public DateTime DateCreated { get; set; } = DateTime.Now;
     }
 }
